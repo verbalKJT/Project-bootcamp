41a25236111d8025f7f98e654f6e0b7006b079f4
KimKyungJun <1124kyungjun@naver.com>
2026-01-10 16:55:11 +0900

    씬 기본 구성 1안

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;// 싱글톤
+
+    void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject); // 씬 넘어가도 삭제 안되도록
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +20,15 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+    // 로비 이동
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+    // 게임 종료
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
