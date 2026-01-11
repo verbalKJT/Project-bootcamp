0ffc8f80ad611f1aeec4463070a7aae0e47a5114
KimKyungJun <1124kyungjun@naver.com>
2026-01-11 20:19:03 +0900

    로비 씬 UI 초안

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,15 +1,6 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;// 싱글톤
-
-    void Awake()
-    {
-        instance = this;
-        DontDestroyOnLoad(gameObject); // 씬 넘어가도 삭제 안되도록
-    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,15 +11,5 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
-    }
-    // 로비 이동
-    public void GameStart()
-    {
-        SceneManager.LoadScene("Lobby");
-    }
-    // 게임 종료
-    public void ExitGame()
-    {
-        Application.Quit();
     }
 }
