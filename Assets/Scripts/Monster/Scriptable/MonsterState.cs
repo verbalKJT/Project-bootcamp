e54392cd57dbeeed6686f66bbb3525e038d1706f
김진태 <verbal0719@naver.com>
2026-02-08 22:32:58 +0900

    Revert "플레이어 공격,점프 + 몬스터 체력"

--- Assets/Scripts/Monster/Scriptable/MonsterState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "MonsterState", menuName = "Scriptable Objects/MonsterState")]
-public class MonsterState : ScriptableObject
-{
-    public float moveSpeed = 20f;
-    public int hp = 100;
-}
