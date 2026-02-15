4a7f4a9b63736e0ff850a2f99fe1dd74be09593c
kimjintae <verbal0719@naver.com>
2026-02-15 23:59:47 +0900

    플레이어 공격 애니메이션에 이벤트 등록

--- Assets/Scripts/Monster/AnimEventMover.cs
+++ Assets/Scripts/Monster/AnimEventMover.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class AnimEventMover : MonoBehaviour
 {
-    private EnemyHealth enemyHealth;
-    void Start()
-    {
-        enemyHealth = GetComponentInParent<EnemyHealth>();
-    }
     
     public void EndDeadAnim()
     {
+        EnemyHealth enemyHealth = GetComponentInParent<EnemyHealth>();
         // 이벤트 발동 시 EnemyHealth의 메소드 호출
         enemyHealth.DestroyObj();
+    }
+
+    public void NatureCannonFire()
+    {
+        Debug.Log("NatureCannonFire");
+        NatureManAttack natureManAttack = GetComponentInParent<NatureManAttack>();
+        natureManAttack.Fire();
     }
 }
