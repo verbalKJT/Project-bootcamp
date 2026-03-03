5605d8f4eda688803f01c76727e13c87584fbad8
KimKyungJun <1124kyungjun@naver.com>
2026-03-03 20:17:32 +0900

    불맨컨트롤러 스킬 구조 초안

--- Assets/Scripts/Player/FireManAttack.cs
+++ Assets/Scripts/Player/FireManAttack.cs
@@ -19,12 +19,26 @@ public class FireManAttack : PlayerAttack
             StartCoroutine(sword.IncreaseSword(1f));
             photonView.RPC("AttackAnim",RpcTarget.All);
         }
+        
+        // Q 키 스킬 (휠윈드) 추가
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            photonView.RPC("Skill_WheelWin", RpcTarget.All);
+        }
     }
     
-
     [PunRPC]
     public void AttackAnim()
     {
         animator.SetTrigger("Attack");
+    }
+    
+    [PunRPC]
+    public void Skill_WheelWin()
+    {
+        animator.SetTrigger("WheelWin");
+        // 휠윈드는 회전하면서 여러 번 때려야 하므로 별도의 코루틴이나 로직이 필요할 수 있습니다.
+        // 우선은 기본 Sword 코루틴을 재활용하거나 새로 만듭니다.
+        StartCoroutine(sword.IncreaseSword(0.2f)); 
     }
 }
