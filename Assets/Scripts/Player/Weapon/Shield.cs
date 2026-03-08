376414b75ede58b12d23d8371cd262f5acd4063b
kimjintae <verbal0719@naver.com>
2026-03-08 23:32:53 +0900

    방패 막기 로직 추가 예정

---
+++ Assets/Scripts/Player/Weapon/Shield.cs
@@ -0,0 +1,16 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class Shield : MonoBehaviourPun
+{
+    [SerializeField] private GameObject shield; // 방패 하위 Plane
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
