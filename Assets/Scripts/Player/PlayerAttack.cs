a3975ade618c14ccf51ae3b5831e9934c7eb2eb4
kimjintae <verbal0719@naver.com>
2026-03-10 22:11:01 +0900

    불맨 스킬 개발

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -7,8 +7,10 @@ public class PlayerAttack : MonoBehaviourPun
     // 하위 스크립트에서도 사용가능하게 protected
 
     protected Animator animator;
-    protected bool input;
-    
+    protected bool input; // 마우스 기본 공격
+    protected bool commandE;
+    protected bool commandQ;
+    protected bool shift;
     protected void Start()
     {
         animator = GetComponentInChildren<Animator>();
@@ -20,5 +22,12 @@ public class PlayerAttack : MonoBehaviourPun
         if (!photonView.IsMine) return;
 
         input = Input.GetMouseButtonDown(0);
+        
+        // 스킬 q,e로
+        // 눌렀을 때 
+        commandE = Input.GetKeyDown(KeyCode.E);
+        commandQ = Input.GetKeyDown(KeyCode.Q);
+        // 사용할지도 모름 걷기 뛰기 분리할 경우 
+        shift = Input.GetKey(KeyCode.LeftShift);
     }
 }
