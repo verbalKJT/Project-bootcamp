87924f645cd8ad8130cbeb4ce9e4f8bab20b7b66
kimjintae <verbal0719@naver.com>
2026-03-30 10:21:58 +0900

    몬스터 생성 로직 수정 및 플레이어 탐지 및 원거리 공격

---
+++ Assets/Scripts/Monster/Rock.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using Photon.Pun;
+using UnityEngine;
+
+public class Rock : MonoBehaviourPun, IPunInstantiateMagicCallback
+{
+    private int damage;
+    private IPunInstantiateMagicCallback punInstantiateMagicCallbackImplementation;
+    public Rigidbody rb{get ; private set;}
+
+    private EnemyRangeAttack era;
+    void Start()
+    {
+        StartCoroutine(DestroySelf(6f));    
+        rb = GetComponent<Rigidbody>();
+        era = GetComponentInParent<EnemyRangeAttack>();
+    }
+    
+    // IPunInstantiateMagicCallback 콜백 함수
+    public void OnPhotonInstantiate(PhotonMessageInfo info)
+    {
+        // 생성 시 넘어온 데이터 읽기.
+        object[] data = info.photonView.InstantiationData;
+        if (data != null && data.Length > 0)
+        {
+            int parentViewID = (int)data[0];
+            // 자신을 생성한 오브젝트를 포톤뷰 아이디를 확인해 찾기
+            PhotonView pv = PhotonView.Find(parentViewID);
+
+            if (pv != null)
+            {
+                // 모델의 FirPos를 찾아서 위치 저장
+                Transform targetTransform = pv.transform.Find("FirPos");
+                // 자식으로 위치
+                transform.SetParent(targetTransform);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player Hit");           
+        }else if(other.tag == "Shield")
+        {
+            
+        }
+    }
+
+    IEnumerator DestroySelf(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        if(PhotonNetwork.IsMasterClient){
+            PhotonNetwork.Destroy(gameObject);
+        }
+    }
+}
