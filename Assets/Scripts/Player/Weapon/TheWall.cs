ac0b81a1e1b00449b13e2b2a7a4005e80102cbb3
kimjintae <verbal0719@naver.com>
2026-04-06 22:55:19 +0900

    생성 시 효과 추가

--- Assets/Scripts/Player/Weapon/TheWall.cs
+++ Assets/Scripts/Player/Weapon/TheWall.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class TheWall : MonoBehaviourPun
 {
     private float desTime = 5f;
+    
+    [Header("설치 시 효과")]
+    [SerializeField] private GameObject setEffect;
     void Start()
     {
         if (photonView.IsMine)
         {
             StartCoroutine(DestroySelf(desTime));
         }
+        if(setEffect != null)
+            Instantiate(setEffect, transform.position, transform.rotation);
     }
     
     IEnumerator DestroySelf(float time)
@@ -26,6 +31,9 @@ public class TheWall : MonoBehaviourPun
             Rigidbody target = collision.gameObject.GetComponent<Rigidbody>();
             
             target.AddForce(-target.transform.forward * 10f, ForceMode.Impulse);
+        }else if (collision.gameObject.layer == LayerMask.NameToLayer("MonsterWeapon"))
+        {
+            PhotonNetwork.Destroy(collision.gameObject);
         }
     }
 }
