69846725291b63c2abc36d46daee4ccd6c0548fd
kimjintae <verbal0719@naver.com>
2026-04-07 12:26:52 +0900

    공격 오디오 추가

--- Assets/Scripts/Monster/Boss/BossAttack.cs
+++ Assets/Scripts/Monster/Boss/BossAttack.cs
@@ -4,14 +4,12 @@ using UnityEngine.Serialization;
 
 public class BossAttack : MonoBehaviourPun
 {
-    [Header("컴포넌트")]
-    [SerializeField] private BossHp boss;
+    [Header("컴포넌트")] [SerializeField] private BossHp boss;
     [SerializeField] private Animator _animator;
-    
-    [Header("공격 스크립트")]
-    [SerializeField] private FallAttack _fall;
-    [SerializeField] private ElectricWireAttack  _electric;
-    
+
+    [Header("공격 스크립트")] [SerializeField] private FallAttack _fall;
+    [SerializeField] private ElectricWireAttack _electric;
+
     // true면 전깃줄 생성 공격도 같이.
     public bool IsHalf = false;
 
@@ -20,18 +18,25 @@ public class BossAttack : MonoBehaviourPun
     private const float fallCool = 6f;
     private float electricTimer = 18f; // 20
     private const float electricCool = 18f;
-    
+
+    private BossFeedBack feedback;
+
+    void Start()
+    {
+        feedback = GetComponent<BossFeedBack>();
+    }
+
     void Update()
-    {   
+    {
         // 보스 공격 쿨타임은 방장만 재기
-        if(!PhotonNetwork.IsMasterClient) return;
+        if (!PhotonNetwork.IsMasterClient) return;
         fallTimer += Time.deltaTime;
         if (fallTimer >= fallCool)
         {
             photonView.RPC("FallCrystal", RpcTarget.All);
             fallTimer = 0f;
         }
-        
+
         electricTimer += Time.deltaTime;
         if (IsHalf && electricTimer >= electricCool)
         {
@@ -39,25 +44,28 @@ public class BossAttack : MonoBehaviourPun
             electricTimer = 0f;
         }
     }
-    
+
     [PunRPC]
     public void FallCrystal()
     {
+        feedback.CallFallClip();
         _animator.SetTrigger("Fall");
     }
-    
+
     // 애니메이션 이벤트로 원하는 타이밍에 호출
     public void SpawnCrystal()
     {
         _fall.CallFallAttack();
         fallTimer = 0f; // 소환 후 쿨타임 재기.
     }
-    
+
     [PunRPC]
     private void ElectricWire()
     {
+        feedback.CallElecClip();
         _animator.SetTrigger("Electric");
     }
+
     // 애니메이션 이벤트로 원하는 타이밍에 호출
     public void SpwanElectric()
     {
