3bf73356f3aa4678d535d5b440d2aee8efc6b34a
kimjintae <verbal0719@naver.com>
2026-04-07 12:30:51 +0900

    대쉬 로직 수정

--- Assets/Scripts/Player/Fire/FireManAttack.cs
+++ Assets/Scripts/Player/Fire/FireManAttack.cs
@@ -140,11 +140,11 @@ public class FireManAttack : PlayerAttack
                 if (!monsters.Contains(hitId))
                 {
                     monsters.Add(hitId);
-                    if (hits.TryGetComponent(out EnemyHealth  target))
+                    if (hits.TryGetComponent(out LivingEnitiy  target))
                     {
                         target.TakeDamage(30);
                         target.photonView.RPC("Is_Hit", RpcTarget.All);
-                    }   
+                    }
                 }
             }
             
